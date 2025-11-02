945252b2662397a4c5fa50415115e3f59565fc83
tangem-service <gitservice@tangem.com>
2025-11-02 20:31:59 +0400

    update hardhat tasks and networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const MNEMONIC = process.env.MNEMONIC;
 const ACCOUNTS = MNEMONIC ? { "mnemonic": MNEMONIC, "initialIndex": 0 } : "remote";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 task("deploy-base", "Deploys contracts for testing")
   .addParam("pool", "The address of the Aave pool")
@@ -20,22 +21,10 @@ task("deploy-base", "Deploys contracts for testing")
     await forwarder.waitForDeployment();
 
     const TangemYieldProcessor = await ethers.getContractFactory("TangemYieldProcessor");
-    const processor = await TangemYieldProcessor.deploy(msgSender, 100);
+    const processor = await TangemYieldProcessor.deploy("0x37E7e93093AE3A8AAEf4A0D41DBd9c037508eB60", 1500);
     await processor.waitForDeployment();
 
     console.log("TangemYieldProcessor deployed to: ", await processor.getAddress());
-
-    const protocolEntererRole = ethers.id("PROTOCOL_ENTERER_ROLE")
-    const grantTx1 = await processor.grantRole(protocolEntererRole, msgSender);
-    await grantTx1.wait();
-
-    const protocolExiterRole = ethers.id("PROTOCOL_EXITER_ROLE")
-    const grantTx2 = await processor.grantRole(protocolExiterRole, msgSender);
-    await grantTx2.wait();
-
-    const serviceFeeCollectorRole = ethers.id("SERVICE_FEE_COLLECTOR_ROLE")
-    const grantTx3 = await processor.grantRole(serviceFeeCollectorRole, msgSender);
-    await grantTx3.wait();
 
     const TangemYieldModuleFactory = await ethers.getContractFactory("TangemYieldModuleFactory");
     const factory = await TangemYieldModuleFactory.deploy();
@@ -58,6 +47,12 @@ task("deploy-base", "Deploys contracts for testing")
 
     const unpauseTx = await factory.unpause();
     await unpauseTx.wait();
+
+    const revokeTx1 = await factory.revokeRole(implementationSetterRole, msgSender);
+    await revokeTx1.wait();
+
+    const revokeTx2 = await factory.revokeRole(pauserRole, msgSender);
+    await revokeTx2.wait();
 
     console.log("TangemYieldModuleFactory deployed to: ", await factory.getAddress());
   });
@@ -124,7 +119,7 @@ task("enter-protocol", "Enters yield protocol")
     console.log("Protocol entered");
   });
 
-task("grant-backend-roles", "Withdraws user funds")
+task("grant-backend-roles", "Grants backend roles")
   .addParam("processor", "The address of the yield processor")
   .addParam("to", "The address of the account to grant roles to")
   .setAction(async (taskArgs) => {
@@ -147,13 +142,81 @@ task("grant-backend-roles", "Withdraws user funds")
     console.log("Backend roles granted");
   });
 
+task("upgrade-module-implementation", "Deploys new module implementation and sets it to factory")
+  .addParam("pool", "The address of the Aave pool")
+  .addParam("processor", "The address of the yield processor")
+  .addParam("factory", "The address of the yield module factory")
+  .addParam("forwarder", "The address of the Tangem forwarder")
+  .setAction(async (taskArgs) => {
+    await hre.run('compile');
+
+    const msgSender = (await hre.ethers.getSigners())[0].address;
+
+    const poolAddress = taskArgs.pool;
+    const processorAddress = taskArgs.processor;
+    const factoryAddress = taskArgs.factory;
+    const forwarderAddress = taskArgs.forwarder;
+
+    const AaveV3YieldModule = await ethers.getContractFactory("TangemAaveV3YieldModule");
+    const moduleImplementation =
+      await AaveV3YieldModule.deploy(poolAddress, processorAddress, factoryAddress, forwarderAddress);
+    await moduleImplementation.waitForDeployment();
+
+    console.log("New implementation deployed to: ", await moduleImplementation.getAddress());
+
+    const TangemYieldModuleFactory = await ethers.getContractFactory("TangemYieldModuleFactory");
+    const factory = TangemYieldModuleFactory.attach(factoryAddress);
+
+    const implementationSetterRole = ethers.id("IMPLEMENTATION_SETTER_ROLE")
+    const grantTx1 = await factory.grantRole(implementationSetterRole, msgSender);
+    await grantTx1.wait();
+
+    const pauserRole = ethers.id("PAUSER_ROLE")
+    const grantTx2 = await factory.grantRole(pauserRole, msgSender);
+    await grantTx2.wait();
+
+    const pauseTx = await factory.pause();
+    await pauseTx.wait();
+
+    const setTx = await factory.setImplementation(moduleImplementation);
+    await setTx.wait();
+
+    const unpauseTx = await factory.unpause();
+    await unpauseTx.wait();
+
+    const revokeTx1 = await factory.revokeRole(implementationSetterRole, msgSender);
+    await revokeTx1.wait();
+
+    const revokeTx2 = await factory.revokeRole(pauserRole, msgSender);
+    await revokeTx2.wait();
+
+    console.log("New implementation set");
+  });
+
+task("get-effective-balance", "Withdraws user funds")
+  .addParam("module", "The address of the yield module")
+  .addParam("token", "The address of the yield token")
+  .setAction(async (taskArgs) => {
+    await hre.run('compile');
+
+    const yieldModuleAddress = taskArgs.module;
+    const yieldTokenAddress = taskArgs.token;
+
+    const AaveV3YieldModule = await ethers.getContractFactory("TangemAaveV3YieldModule");
+    const yieldModule = AaveV3YieldModule.attach(yieldModuleAddress);
+    
+    const effectiveBalance = await yieldModule.effectiveBalance(yieldTokenAddress);
+
+    console.log("Effective balance - ", effectiveBalance);
+  });
+
 module.exports = {
   solidity: {
     version: "0.8.29",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 10000
       }
     }
   },
@@ -169,8 +232,44 @@ module.exports = {
     flat: true
   },
   networks: {
+    ethereum: {
+      url: "https://mainnet.gateway.tenderly.co",
+      accounts: ACCOUNTS
+    },
+    avalanche: {
+      url: "https://avalanche.drpc.org",
+      accounts: ACCOUNTS
+    },
+    arbitrum: {
+      url: "https://arbitrum-one.public.blastapi.io",
+      accounts: ACCOUNTS
+    },
+    optimism: {
+      url: "https://optimism.drpc.org",
+      accounts: ACCOUNTS
+    },
+    base: {
+      url: "https://base-rpc.publicnode.com",
+      accounts: ACCOUNTS
+    },
+    gnosis: {
+      url: "https://rpc.gnosischain.com",
+      accounts: ACCOUNTS
+    },
+    bsc: {
+      url: "https://bsc.drpc.org",
+      accounts: ACCOUNTS
+    },
+    zksync: {
+      url: "https://zksync.drpc.org",
+      accounts: ACCOUNTS
+    },
     polygon: {
       url: "https://rpc-mainnet.matic.quiknode.pro/",
+      accounts: ACCOUNTS
+    },
+    sonic: {
+      url: "https://sonic.drpc.org",
       accounts: ACCOUNTS
     },
     sepolia: {
@@ -184,6 +283,12 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: false
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: false
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS == "true"
