2b0887986b28f94100bada5fb14a946d0eaa1b30
tangem-service <gitservice@tangem.com>
2026-07-16 18:36:45 +0500

    test: add IncreaseProtocolBalanceWithoutFeeTest

---
+++ test/foundry/MerklIncentives/IncreaseProtocolBalanceWithoutFee.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+/* solhint-disable func-name-mixedcase */
+pragma solidity ^0.8.29;
+
+import { MerklIncentivesBase } from "./MerklIncentivesBase.sol";
+import { IYieldModule } from "contracts/interfaces/IYieldModule.sol";
+
+/// Covers _increaseProtocolBalanceWithoutFee, driven through Merkl claims
+contract IncreaseProtocolBalanceWithoutFeeTest is MerklIncentivesBase {
+    function setUp() public override {
+        super.setUp();
+
+        ym = _deployEnteredRevenueModule(owner);
+    }
+
+    function testFuzz_claim_MovesFeeCheckpoint_OnPushToProtocol(uint amount) public {
+        amount = bound(amount, 1, 1_000_000_000e18);
+
+        (uint checkpointBefore,) = ym.latestFeePaymentStates(address(yieldToken));
+        uint feeBefore = ym.calculateServiceFee(address(yieldToken));
+
+        _fundMerklDistributor(address(yieldToken), amount);
+
+        _claimSingleAsOwner(address(yieldToken), amount);
+
+        (uint checkpointAfter, uint rateAfter) = ym.latestFeePaymentStates(address(yieldToken));
+        assertEq(checkpointAfter, checkpointBefore + amount);
+        assertEq(rateAfter, SERVICE_FEE_RATE);
+        // the checkpoint moved together with the balance: the reward accrues no fee
+        assertEq(ym.calculateServiceFee(address(yieldToken)), feeBefore);
+    }
+
+    function testFuzz_claim_MovesFeeCheckpoint_OnKeepInModule(uint amount) public {
+        amount = bound(amount, 1, 1_000_000_000e18);
+
+        (uint checkpointBefore,) = ym.latestFeePaymentStates(address(yieldToken));
+        uint feeBefore = ym.calculateServiceFee(address(yieldToken));
+
+        _fundMerklDistributor(address(protocolToken), amount);
+
+        _claimSingleAsOwner(address(protocolToken), amount);
+
+        (uint checkpointAfter, uint rateAfter) = ym.latestFeePaymentStates(address(yieldToken));
+        assertEq(checkpointAfter, checkpointBefore + amount);
+        assertEq(rateAfter, SERVICE_FEE_RATE);
+        assertEq(ym.calculateServiceFee(address(yieldToken)), feeBefore);
+    }
+
+    function test_claim_Reverts_WhenFeeCheckpointExceedsBalance() public {
+        // checkpoint above the real protocol balance, e.g. after a position loss
+        uint protocolBalance = ym.protocolBalance(address(yieldToken));
+        ym.exposed_setLatestFeePaymentState(
+            address(yieldToken), protocolBalance + 1, SERVICE_FEE_RATE
+        );
+
+        _fundMerklDistributor(address(protocolToken), AMOUNT);
+
+        vm.expectRevert(IYieldModule.FeeCheckpointExceedsBalance.selector);
+
+        _claimSingleAsOwner(address(protocolToken), AMOUNT);
+    }
+}
