ae0233ed80327d4b0093bdde7b05b345d8723d61
tangem-service <gitservice@tangem.com>
2026-07-28 15:27:13 +0500

    fix: interface

--- contracts/interfaces/IYieldProcessor.sol
+++ contracts/interfaces/IYieldProcessor.sol
@@ -5,5 +5,5 @@ interface IYieldProcessor {
     function feeReceiver() external view returns (address);
 
     // rate is specified in basis points (0.01 %)
-    function serviceFeeRate() external returns (uint);
+    function serviceFeeRate() external view returns (uint);
 }
