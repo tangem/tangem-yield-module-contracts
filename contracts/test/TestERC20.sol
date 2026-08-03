ec1050bf80ddfccc4f4e80a275862d379de77465
tangem-service <gitservice@tangem.com>
2026-08-03 17:28:14 +0500

    fix: solhint errors

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
     uint8 private immutable _decimals;
